8a920024e24c89191bc95feceb5df9f853b0d576
Kevin.S <k.singhateh.pro@gmail.com>
2023-10-22 00:17:54 +0200

    admin rdv

--- src/Controller/AdminDashboardController.php
+++ src/Controller/AdminDashboardController.php
@@ -2,17 +2,27 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\PersonneRepository;
+use App\Repository\RendezVousRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AdminDashboardController extends AbstractController
 {
     #[Route('/admin/dashboard', name: 'app_admin_dashboard')]
-    public function index(): Response
+    public function index(RendezVousRepository $rdvRepository, PersonneRepository $personneRepository): Response
     {
+
+        $rendezVous = $rdvRepository->findAll();
+        $personne = $personneRepository->findAll();
+
+
+
         return $this->render('admin_dashboard/index.html.twig', [
             'controller_name' => 'AdminDashboardController',
+            'rendezVous' => $rendezVous,
+            'personne' => $personne
         ]);
     }
 }
