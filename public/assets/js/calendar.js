c4d3210f7f498f6e909e8589f6c8db4ef7260e39
rojdi59 <cr7etkb9.rh@gmail.com>
2023-11-09 09:03:48 +0100

    nouveau projet

--- public/assets/js/calendar.js
+++ public/assets/js/calendar.js
@@ -15,16 +15,26 @@ $(function (e) {
     }).datepicker('update', new Date());
 
     //Time Picker
-<<<<<<< HEAD
     $('#tp4').timepicker();
 
     $(document).on('click', '#setTimeButton', function () {
         $('#tp4').timepicker('setTime', new Date());
-=======
-    $('#tp3').timepicker();
-
-    $(document).on('click', '#setTimeButton', function () {
-        $('#tp3').timepicker('setTime', new Date());
->>>>>>> main
     });
 })
+$('#calendar').fullCalendar({
+    header: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'month,agendaWeek,agendaDay'
+    },
+    businessHours: {
+      // Configuration des heures de travail pour les jours de la semaine
+      dow: [1, 2, 3], // Lundi, Mardi, Mercredi
+      start: '08:00', // Heure de début (8:00 AM)
+      end: '18:00' // Heure de fin (6:00 PM)
+    },
+    events: [
+      // Insérez ici vos événements
+    ]
+  });
+  
